9041079bbc065713ff578d737e64cb5d595468f0
Moh Syaifudin <moh.syaifudin@mail.ugm.ac.id>
2022-10-21 19:44:08 +0700

    add files

---
+++ Output Awal - Statistik Bottom 10.r
@@ -0,0 +1,5 @@
+library(arules)
+data <- read.transactions(file = "transaksi_dqlab_retail.tsv", format = "single", sep = "\t", cols = c(1,2), skip = 1 )
+bottom_10 <- sort(itemFrequency(data, type="absolute"), decreasing = FALSE)[1:10]
+hasil <- data.frame("Nama Produk" = names(bottom_10), "Jumlah" = bottom_10, row.names = NULL)
+write.csv(hasil, file="bottom10_item_retail.txt")
